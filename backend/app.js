66583425d86270856e3734670ca51056e4024c4d
SebMZI <mrgy.sebastien@gmail.com>
2025-04-09 16:00:43 +0200

    feat: created user model

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,12 @@
 import express from "express";
+import cookieparser from "cookie-parser";
 import authRouter from "./routes/auth.routes.js";
 import { connectToDatabase } from "./config/database.js";
 import { PORT } from "./config/env.js";
 
 const app = express();
+app.use(express.json());
+app.use(cookieparser());
 
 app.use("/auth", authRouter);
 
---
+++ backend/models/user.model.js
@@ -0,0 +1,36 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  firstName: {
+    type: String,
+    required: [true, "User FirstName is required"],
+    minLength: 3,
+    maxLength: 50,
+  },
+  lastName: {
+    type: String,
+    required: [true, "User LastName is required"],
+    minLength: 3,
+    maxLength: 50,
+  },
+  emai: {
+    type: String,
+    required: [true, "User Email is required"],
+    unique: true,
+    match: /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g,
+  },
+  username: {
+    type: String,
+    required: [true, "User Username is required"],
+    minLength: 3,
+    maxLength: 50,
+  },
+  password: {
+    type: String,
+    required: [true, "User Password is required"],
+  },
+});
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
